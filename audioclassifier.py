c68ed5ceb8dc3fc8d70b39362016b821171d5476
manvik S <manvik55@gmail.com>
2022-07-05 14:02:01 +0530

    single batch prediction

--- audioclassifier.py
+++ audioclassifier.py
@@ -1,7 +1,10 @@
 import os
 from matplotlib import pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
 import tensorflow as tf
 import tensorflow_io as tfio
+
 
 #paths to file
 CAPUCHIN_FILE = os.path.join('Parsed_Capuchinbird_Clips', 'XC3776-3.wav')
@@ -40,13 +43,12 @@ positives = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.ones
 negatives = tf.data.Dataset.zip((neg, tf.data.Dataset.from_tensor_slices(tf.zeros(len(neg)))))
 data = positives.concatenate(negatives)
 
-#calculate wave cycle length
+#calculate wave cycle length(min found to be 32000 max=80000)
 lengths = []
 for file in os.listdir(os.path.join('Parsed_Capuchinbird_Clips')):
     tensor_wave = load_wav_16k_mono(os.path.join('Parsed_Capuchinbird_Clips', file))
     lengths.append(len(tensor_wave))
-
-#preprocessing function to convert to spectrogram
+#preprocessing function to convert to spectrogram using 48000 length and padding with 0 at start if  length of audio is lesser
 def preprocess(file_path, label): 
     wav = load_wav_16k_mono(file_path)
     wav = wav[:48000]
@@ -63,3 +65,53 @@ spectrogram, label = preprocess(filepath, label)
 plt.figure(figsize=(30,20))
 plt.imshow(tf.transpose(spectrogram)[0])
 plt.show()
+
+#create tensorflow data pipeline
+data = data.map(preprocess)
+data = data.cache()
+data = data.shuffle(buffer_size=1000)
+data = data.batch(16)
+data = data.prefetch(8)
+
+#split into trian and testing
+train = data.take(36)
+test = data.skip(36).take(15)
+
+#test one batch
+# samples, labels = train.as_numpy_iterator().next()
+# samples.shape         gives shape (16,1491,257,1)
+
+#build sequential model
+model = Sequential()
+model.add(Conv2D(16, (3,3), activation='relu', input_shape=(1491, 257,1)))
+model.add(Conv2D(16, (3,3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile('Adam', loss='BinaryCrossentropy', metrics=[tf.keras.metrics.Recall(),tf.keras.metrics.Precision()])
+model.summary()
+
+#fit model, view loss and KPI plots
+hist = model.fit(train, epochs=4, validation_data=test)
+
+plt.title('Loss')
+plt.plot(hist.history['loss'], 'r')
+plt.plot(hist.history['val_loss'], 'b')
+plt.show()
+
+plt.title('Precision')
+plt.plot(hist.history['precision'], 'r')
+plt.plot(hist.history['val_precision'], 'b')
+plt.show()
+
+plt.title('Recall')
+plt.plot(hist.history['recall'], 'r')
+plt.plot(hist.history['val_recall'], 'b')
+plt.show()
+
+#make prediction on 1 batch
+X_test, y_test = test.as_numpy_iterator().next()
+yhat = model.predict(X_test)
+
+#convert logits to classes
+yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]

66e7b6b78e3dfc9e2b3e8a3101762b0182418ee0
manvik S <manvik55@gmail.com>
2022-07-05 09:30:35 +0530

    visualize spectrogram

--- audioclassifier.py
+++ audioclassifier.py
@@ -26,3 +26,40 @@ nwave = load_wav_16k_mono(NOT_CAPUCHIN_FILE)
 plt.plot(wave)
 plt.plot(nwave)
 plt.show()
+
+#path to positive and negative data
+POS = os.path.join('Parsed_Capuchinbird_Clips')
+NEG = os.path.join('Parsed_Not_Capuchinbird_Clips')
+
+#create tensorflow dataset
+pos = tf.data.Dataset.list_files(POS+'\*.wav')
+neg = tf.data.Dataset.list_files(NEG+'\*.wav')
+
+#add label as 1 for positive and 0 for negative samples
+positives = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
+negatives = tf.data.Dataset.zip((neg, tf.data.Dataset.from_tensor_slices(tf.zeros(len(neg)))))
+data = positives.concatenate(negatives)
+
+#calculate wave cycle length
+lengths = []
+for file in os.listdir(os.path.join('Parsed_Capuchinbird_Clips')):
+    tensor_wave = load_wav_16k_mono(os.path.join('Parsed_Capuchinbird_Clips', file))
+    lengths.append(len(tensor_wave))
+
+#preprocessing function to convert to spectrogram
+def preprocess(file_path, label): 
+    wav = load_wav_16k_mono(file_path)
+    wav = wav[:48000]
+    zero_padding = tf.zeros([48000] - tf.shape(wav), dtype=tf.float32)
+    wav = tf.concat([zero_padding, wav],0)
+    spectrogram = tf.signal.stft(wav, frame_length=320, frame_step=32)
+    spectrogram = tf.abs(spectrogram)
+    spectrogram = tf.expand_dims(spectrogram, axis=2)
+    return spectrogram, label 
+
+#Visualize spectrogram
+filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
+spectrogram, label = preprocess(filepath, label)
+plt.figure(figsize=(30,20))
+plt.imshow(tf.transpose(spectrogram)[0])
+plt.show()
